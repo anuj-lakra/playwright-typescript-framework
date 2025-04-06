c300adfb82a57a7d8654e2289b5abd3fd150326c
Anuj Lakra <anuj.samuellakra@gmail.com>
2025-04-06 15:14:35 -0700

    add eslint

--- src/utilities/accessibility.ts
+++ src/utilities/accessibility.ts
@@ -1,7 +1,8 @@
 import AxeBuilder from "@axe-core/playwright";
 import { Page } from "@playwright/test";
+import { AxeResults } from "axe-core"; // Import the type for Axe results
 
-export async function runAccessibilityScan(page: Page) {
+export async function runAccessibilityScan(page: Page): Promise<AxeResults> {
   const accessibilityScanResults = await new AxeBuilder({ page })
     .withTags([
       "wcag2a",
